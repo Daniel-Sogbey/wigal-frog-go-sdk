e83f61faeb56b8c24b161bf3952e0ddfc8b2b12c
Daniel-Sogbey <sogbeydaniel28@gmail.com>
2025-09-14 16:55:40 +0000

    fix: removed config initializer

--- config.go
+++ config.go
@@ -5,11 +5,3 @@ type Config struct {
 	SenderId string
 	Username string
 }
-
-func NewConfig(apiKey, senderId, username string) *Config {
-	return &Config{
-		ApiKey:   apiKey,
-		SenderId: senderId,
-		Username: username,
-	}
-}
